2dd35284c1885f9f812f5eaab6e50dc713b76abc
Korshunov Evgenyi <korhynov@mail.ru>
2023-09-28 07:36:22 +0300

    branching/rebase.sh 28.09

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,9 +2,16 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
 for param in "$@"; do
     echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
 
 echo "====="
+=======
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
+    count=$(( $count + 1 ))
+done
+>>>>>>> e4664f8 (prepare for merge and rebase)
