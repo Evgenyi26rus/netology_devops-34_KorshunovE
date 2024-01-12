1fdf8cbc36100bccbbd29f63e6bfae23f320f7a0
korshunovi <korhynov@mail.ru>
2024-01-12 22:26:31 +0300

    DZ_4.2.2

--- DZ_4_Terraform/DZ_4.2/src/variables.tf
+++ DZ_4_Terraform/DZ_4.2/src/variables.tf
@@ -39,3 +39,35 @@ variable "vms_ssh_root_key" {
   default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXcX/skkKcv7hhUrGdQSLU/UrWzBAeJpSQki637qeQv"
   description = "ssh-keygen -t ed25519"
 }
+
+### + DZ 4.2.2
+
+variable "vm_web_family" {
+  type = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "vm_web_name" {
+  type = string
+  default = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  type = string
+  default = "standard-v1"
+}
+
+variable "vm_web_cores" {
+  type = number
+  default = 2
+}
+
+variable "vm_web_memory" {
+  type = number
+  default = 1
+}
+
+variable "vm_web_core_fraction" {
+  type = number
+  default = 5
+}
